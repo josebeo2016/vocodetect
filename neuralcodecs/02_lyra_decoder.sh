e77281101579070b6a6db60de9d9bd2232cc1bd8
josebeo2016 <josebeo2016@gmail.com>
2024-01-14 23:00:48 +0900

    add lyra tool

---
+++ neuralcodecs/02_lyra_decoder.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# This script is used to decode the audio files in the specified directory using the Lyra decoder.
+# Usage: ./02_lyra_decoder.sh <path_to_feats> <path_to_output_directory>
+
+INPUT_DIR=$1
+OUTPUT_DIR=$2
+BITRATE=3200
+
+# check if the number of arguments is correct
+if [ $# -ne 2 ]; then
+    echo "Usage: ./02_lyra_decoder.sh <path_to_feats> <path_to_output_directory>"
+    exit 1
+fi
+
+# check if the input directory exists
+if [ ! -d "$INPUT_DIR" ]; then
+    echo "Input directory does not exist."
+    exit 1
+fi
+
+# create the output directory if it does not exist
+if [ ! -d "$OUTPUT_DIR" ]; then
+    mkdir -p "$OUTPUT_DIR"
+fi
+
+filelist=$(ls $INPUT_DIR/*.lyra)
+filecount=$(ls $INPUT_DIR/*.lyra | wc -l)
+# decode the audio files
+cd lyra/
+counter=0
+for file in $filelist
+do
+    # echo "decoding $file"
+    bazel-bin/lyra/cli_example/decoder_main --encoded_path $file --output_dir $OUTPUT_DIR  --bitrate=3200 > /dev/null 2>&1
+    counter=$((counter+1))
+    percent=$((100*counter/filecount))
+    printf "\rProgress: [%-50s] %d%%, processed %d out of %d files." $(printf '%.0s#' $(seq 1 $((percent/2)))) $percent $counter $filecount
+done
